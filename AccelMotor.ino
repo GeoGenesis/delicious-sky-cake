81355d5edc7bbb3ccd03d1f5f78ece22b364bdba
GeoGenesis <silent.emo13@gmail.com>
2016-03-29 18:25:11 +0100

    Added files via upload

---
+++ AccelMotor.ino
@@ -0,0 +1,82 @@
+/*
+ * Code written by GEORGE GOODSELL
+ * Student ID: 120015759
+ * City University, 2016
+ * THESIS WORK
+ */
+
+#include <Servo.h>
+#include <Wire.h>
+
+#define accel_module (0x53)
+
+Servo servoMain; // Servo Define
+
+int SERIAL_REFRESH_RATE = 9600;
+int PIN = 10; 
+
+byte accelValues[6];
+char output[512];
+
+//====================== MAIN FUNCTIONS =============================\\
+
+void setup()
+{
+  
+   servoMain.attach(PIN); // servo on digital pin 10
+   Wire.begin();
+   Serial.begin(SERIAL_REFRESH_RATE);
+}
+
+
+void loop()
+{
+  servoTest();
+//  wiper(0);
+}
+
+//====================== ADDITIONAL FUNCTIONS =============================\\
+
+void servoTest() {
+   servoMain.write(45);  // Turn Servo Left to 45 degrees
+   delay(1000);          // Wait 1 second
+   servoMain.write(0);   // Turn Servo Left to 0 degrees
+   delay(1000);          // Wait 1 second
+   servoMain.write(90);  // Turn Servo back to center position (90 degrees)
+   delay(1000);          // Wait 1 second
+   servoMain.write(135); // Turn Servo Right to 135 degrees
+   delay(1000);          // Wait 1 second
+   servoMain.write(180); // Turn Servo Right to 180 degrees
+   delay(1000);          // Wait 1 second
+   servoMain.write(90);  // Turn Servo back to center position (90 degrees)
+   delay(5000);          // Wait 5 seconds
+  }
+
+
+  void wiper(int x) {
+
+    String partA = String("Angle: ");
+    String partB = String(x);
+    String output (partA + partB);
+    
+    Serial.println(partA); // output to serial monitor
+    
+    servoMain.write(x); // Set to angle x
+    Serial.println(partB);
+    delay(500);
+
+    x = x+180;
+    
+    servoMain.write(x);
+    Serial.println(output);
+    delay(500);
+    
+    servoMain.write(x-180);
+    delay(500);
+    }
+
+void serialHandler() {
+
+  
+  
+  }
