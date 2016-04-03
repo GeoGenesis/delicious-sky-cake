ec175487d742cffe0c5bf6c4db2867627f6cc292
GeoGenesis <silent.emo13@gmail.com>
2016-04-03 11:50:15 +0100

    Added files via upload
    Controls Servo

--- MotorControl.ino
+++ MotorControl.ino
@@ -1,28 +1,53 @@
+/*
+ * Code written by GEORGE GOODSELL
+ * Student ID: 120015759
+ * City University, 2016
+ * THESIS WORK
+ */
+
 #include <Servo.h>
+#include <Wire.h>
 
-Servo servoControl;
+Servo servoMain; // Servo Define
 
-//Variables
-int const potPin = A0; // Potentiometer Control
-int potVal; // Potentiometer Value
-int angle; // Set angle of Motor
+int SERIAL_REFRESH_RATE = 9600;
+int PIN = 10; 
 
-void setup() {
-  servoControl.attach(9); // Attaching Servo to pin 9 on Shield
-  Serial.begin(9600);
+//====================== MAIN FUNCTIONS =============================\\
+
+void setup()
+{
+   servoMain.attach(PIN); // servo on digital pin 10
+   Serial.begin(SERIAL_REFRESH_RATE);
 }
 
-void loop() {
-  potVal = analogRead(potPin);
-  Serial.print("Potentiometer Value: ");
-  Serial.print(potVal);
+void loop()
+{
+  servoDiag();
+//  wiper(0);
+}
 
-  angle = map(potVal, 0, 1023, 0, 179); // Mapping Potentiometer Value to angle value (1-180);
-  Serial.print(", angle: ");
-  Serial.print(angle);
+//====================== ADDITIONAL FUNCTIONS =============================\\
 
-  angle = map(potVal , 0, 1023, 0, 179);
-  servoControl.write(angle);
-  delay(15); // allows time for servo to move to new position
+void servoDiag() {
+   int D = 1000;
+   
+   servoWrite(0, D);      // Turn Servo Left to 0 degrees        
+   servoWrite(45, D);     // Servo to 45
+   servoWrite(90, D);     // Servo to 90
+   servoWrite(135, D);    // Servo to 135
+   servoWrite(180, D);    // Servo to 180
+   servoWrite(90, 5000);  // Servo to midpoint and long pause
 
 }
+
+void servoWrite(int angle, int Delay) { 
+  servoMain.write(angle);        // Rotate Servo to specified angle
+  serialOut(servoMain.read());   // Print Servo angle to Serial Monitor
+  delay(Delay);                   // Wait 1 second (1000 Milliseconds)
+}
+
+void serialOut(int angleMeasure) {
+  Serial.print("Current Angle:    ");
+  Serial.println(angleMeasure);
+  }
